320fbbfe2a3f7e760befe86fe5b5e1f9c4135cd3
Sarah Tumenas <sarah.marques@spread.com.br>
2022-09-13 14:17:09 -0300

    aumentando a complexidade

--- src/main/java/br/com/alura/threads/compartilhandobanheiro/Principal.java
+++ src/main/java/br/com/alura/threads/compartilhandobanheiro/Principal.java
@@ -9,8 +9,15 @@ public class Principal {
         Thread convidado1 = new Thread(new TarefaNumero1(banheiro), "João");
         Thread convidado2 = new Thread(new TarefaNumero2(banheiro), "Pedro");
 
+        //outros threads
+        Thread convidado3 = new Thread(new TarefaNumero1(banheiro), "Maria");
+        Thread convidado4 = new Thread(new TarefaNumero2(banheiro), "Ana");
+
         convidado1.start();
         convidado2.start();
+
+        convidado3.start();
+        convidado4.start();
     }
 
 }
