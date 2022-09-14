4959859c2e1e852977e85729df75c2265f86cf19
Sarah Tumenas <sarah.marques@spread.com.br>
2022-09-14 09:35:57 -0300

    Listas sincronizadas -thread safe - Vector

--- src/main/java/br/com/alura/threads/listas/Principal.java
+++ src/main/java/br/com/alura/threads/listas/Principal.java
@@ -3,12 +3,14 @@ package br.com.alura.threads.listas;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Vector;
 
 public class Principal {
 
     public static void main(String[] args) throws InterruptedException {
 
-        List<String> lista = Collections.synchronizedList(new ArrayList<String>());
+        List<String> lista = new Vector<String>();
+
 
         for (int i = 0; i < 10; i++) {
             new Thread(new TarefaAdicionarElemento(lista, i)).start();
